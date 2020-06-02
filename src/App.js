5cd8da2823a31da2fb48c87c5ec51d8c88342213
conrad96 <conrad.mugisha@gmail.com>
2020-06-02 12:31:40 +0300

    added functions to get search input field value and query recipe api

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, {useEffect, useState, Component} from 'react'
 import Recipe from './recipe';
 
 const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken');
 
   const APP_ID = "bd78a031";
   const APP_KEY = "38e334f82af3507427db26b5a4fdd81a";
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-
-  const [recipes, setRecipes] = useState([]);
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   useEffect(()=> {
 
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const respose = await fetch(url);
@@ -27,10 +27,19 @@ const App = () => {
     fetch(url);
   }
 
+  const updateSearch = (e) => {
+    setSearch(e.target.value);    
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
   return (
     <div className="App">
-      <form className="search-form">
-        <input type="text" />
+      <form onSubmit={getSearch} className="search-form">
+        <input type="text" value={search} onChange={updateSearch} />
         <button type="submit" className="search-button">
           Search
         </button>
