cd40cf8d21e74e786364ad8a991fc89bb8765499
conrad96 <conrad.mugisha@gmail.com>
2020-06-02 06:18:54 +0300

    added functions to fetch data from recipe-api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState, Component} from 'react'
 
 const App = () => {
 
@@ -6,10 +6,26 @@ const App = () => {
   const APP_KEY = "38e334f82af3507427db26b5a4fdd81a";
   const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  useEffect(()=> {
+
+  }, []);
+
+  const getRecipes = async () => {
+    const respose = await fetch(url);
+    const data = await respose.json();
+    console.log(data);
+
+    fetch();
+  }
 
   return (
     <div className="App">
-      <h1>Recipe Application</h1>
+      <form className="search-form">
+        <input type="text" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
