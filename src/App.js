c068de93c0d5256a2463a39e885c890414fde728
conrad96 <conrad.mugisha@gmail.com>
2020-06-02 05:57:40 +0300

    use sample GET url with params of recipe api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const App = () => {
-  
+
   const APP_ID = "bd78a031";
   const APP_KEY = "38e334f82af3507427db26b5a4fdd81a";
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
 
   return (
     <div className="App">
